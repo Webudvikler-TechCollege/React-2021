1544c7ca4c39d2e6a0660bd03eb23b14c8fc23c4
Heinz K <heka@techcollege.dk>
2021-01-14 00:35:17 +0100

    Update

--- heka/src/App.js
+++ heka/src/App.js
@@ -1,26 +1,16 @@
 import './App.scss';
 import Header from './components/Partials/Header';
 import Footer from './components/Partials/Footer';
-import Section from './components/Elements/Section/Section';
+import Nav from './components/Partials/Nav';
 
-const arrSections = [
-  {id: 1, header: 'HVAD ER CO2?', class: 'box-brown', image: 'Icon-CO2.png'},
-  {id: 2, header: 'KLIMA OG MILJØ?', class: 'box-blue', image: 'Icon-Climate.png'},
-  {id: 3, header: 'HVORDAN KAN JEG BIDRAGE?', class: 'box-green', image: 'Icon-Checklist.png'},
-  {id: 4, header: 'DELTAG I ARRANGEMENT', class: 'box-red', image: 'Icon-Arrangement.png'}
-]
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header>
+        <Nav></Nav>
+      </Header>
       <main>
-      {arrSections.map((obj, key) => {
-        return (
-          <Section key={obj.id} data={obj}></Section>
-        )
-        })
-      }
       </main>
       <Footer />
     </div>
--- heka/src/components/Partials/Header.jsx
+++ heka/src/components/Partials/Header.jsx
@@ -1,21 +1,10 @@
 import styles from "./Header.module.scss";
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <div>
-            <section className={`${styles.hero} ${styles.flex}`}>
-                <div>
-                    <a href="index.html"><img src="/images/layout/KT-Logo.png" alt="Logo" /></a>
-                </div>            
-                <article>
-                    <p>"Det er rigtigt, at vi hver især kun bidrage lidt til den samlede løsning"</p>
-                    <p>"Lige så vigtigt er det, at vi slet ikke kan nå målet, uden at hver især bidrager en smule til det"</p>
-                </article>            
-            </section>        
-            <header>
-                <nav id="nav"></nav>
-            </header>  
-        </div>        
+        <header>
+            {props.children}
+        </header>  
     )
 }
 
