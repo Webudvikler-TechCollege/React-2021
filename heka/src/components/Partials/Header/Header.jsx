1eb382d1a4da84a9e209bd3b971474cd8338f0f5
Heinz K <heka@techcollege.dk>
2021-01-19 07:03:39 +0100

    Ny struktur

--- heka/src/components/Partials/Header/Header.jsx
+++ heka/src/components/Partials/Header/Header.jsx
@@ -1,10 +1,12 @@
 import styles from "./Header.module.scss";
 import { Nav } from '../Nav/Nav';
+import { Slider } from '../../Elements/Slider/Slider'
 
 export const Header = (props) => {
     return (
         <header>
             <Nav />
+            <Slider />
         </header>  
     )
 }
