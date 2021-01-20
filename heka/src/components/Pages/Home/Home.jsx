a5be58366fa519457c6f41b40776f23eac8fddfb
Heinz K <heka@techcollege.dk>
2021-01-20 07:47:26 +0100

    Project Updates

--- heka/src/components/Pages/Home/Home.jsx
+++ heka/src/components/Pages/Home/Home.jsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from 'react';
 
 export const Home = () => {
 
+    const [apiData, setApiData] = useState(0);
     const url = "https://api.mediehuset.net/bakeonline/news";
 
-    
-
-
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+      
+    fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(result => setApiData(result))
+        .catch(error => console.log('error', error));
 
     return (
         <>
